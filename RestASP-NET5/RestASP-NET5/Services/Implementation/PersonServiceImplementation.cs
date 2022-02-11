b00bd8b86066859569bef3dbddc5c9aa53864c83
Bruno Henrique <bruno_hcs2009@hotmail.com>
2022-02-10 22:14:07 -0300

    Quatros verbos principais
    GET, POST, PUT e DELETE

---
+++ RestASP-NET5/RestASP-NET5/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,68 @@
+﻿using RestASP_NET5.Model;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestASP_NET5.Services.Implementation
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+
+            return people;
+        }
+  
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Bruno",
+                LastName = "Henrique",
+                Address = "São Paulo",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
--- RestASP-NET5/RestASP-NET5/Startup.cs
+++ RestASP-NET5/RestASP-NET5/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestASP_NET5.Services;
+using RestASP_NET5.Services.Implementation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,10 @@ namespace RestASP_NET5
         {
 
             services.AddControllers();
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestASP_NET5", Version = "v1" });
